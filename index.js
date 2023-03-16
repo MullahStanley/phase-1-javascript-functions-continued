b282809c7948f94f9c4437f6c195fa3f7c5b6ba3
MullahStanley <mullah.kimani@student.moringaschool.com>
2023-03-16 15:24:09 +0300

    Assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// code your solution here
+//saturdayFun function
+function saturdayFun(action){
+    if(action==null){
+    return 'This Saturday, I want to roller-skate!'
+    }else{
+    return `This Saturday, I want to ${action}!`
+    }
+}
+saturdayFun();
+//mondayWork function
+function mondayWork(string){
+    if(string==null){
+        return 'This Monday, I will go to the office.'
+    }else{
+        return `This Monday, I will ${string}.`
+    }
+}
+mondayWork();
+//wrapAdjective function
+function wrapAdjective(style){
+    if(style=='*'){
+        return function(){
+            return `You are ${style}a hard worker${style}!`
+        }
+    }else if(style=='||'){
+        return function(sentence){
+            return `You are ${style}a dedicated programmer${style}!`
+        }
+    }
+}
